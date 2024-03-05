c70c492befffd5f078e35c662faeb6d41f6d4792
kpriyanshu2003 <az.priyanshu.rockstar@gmail.com>
2024-03-06 00:23:16 +0530

    Update Cards.ts, tailwind.config.ts, page.tsx, and Card.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,9 @@
 import About from "@/components/About";
-import Sentiment from "@/components/Sentiment";
-import Team from "@/components/Team";
-import TradingView from "@/components/TradingViewWidget";
 
 export default function Home() {
   return (
     <div>
-      {/* <TradingView /> */}
-      {/* <Sentiment /> */}
-      {/* <About /> */}
-      <Team />
+      <About />
     </div>
   );
 }
