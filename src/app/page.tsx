53d5d1ce11b662cd8ea728f162997c2c0ec49849
kpriyanshu2003 <az.priyanshu.rockstar@gmail.com>
2024-03-05 16:16:11 +0530

    Skeleton for About Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import About from "@/components/About";
 import Sentiment from "@/components/Sentiment";
 import TradingView from "@/components/TradingViewWidget";
 
@@ -5,7 +6,8 @@ export default function Home() {
   return (
     <div>
       {/* <TradingView /> */}
-      <Sentiment />
+      {/* <Sentiment /> */}
+      <About />
     </div>
   );
 }
--- src/ui/Card.tsx
+++ src/ui/Card.tsx
@@ -1,8 +1,9 @@
+import { CardImageContainerProps } from "@/@types/Cards";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
-export function CardContainer({
-  children,
-}: Readonly<{ children: React.ReactNode }>) {
+export function CardContainer({ children }: { children: React.ReactNode }) {
   return <div className="p-4">{children}</div>;
 }
 
@@ -12,20 +13,41 @@ export function CardTitle({
   return <h3 className="text-2xl font-medium">{children}</h3>;
 }
 
-export function CardContent({
-  children,
-}: Readonly<{ children: React.ReactNode }>) {
+export function CardContent({ children }: { children: React.ReactNode }) {
   return <>{children}</>;
 }
 
 export function CardSubHeading({
   children,
   icon,
-}: Readonly<{ children: React.ReactNode; icon: string }>) {
+}: Readonly<{ children: React.ReactNode }> & { icon?: string }) {
   return (
     <h3 className="text-xl">
       <>{children}</>
       {icon && <> &emsp; {icon}</>}
     </h3>
+  );
+}
+
+export function CardImageContainer({
+  buttonText,
+  buttonLink,
+  text,
+  image,
+}: CardImageContainerProps) {
+  return (
+    <div className="p-3">
+      <Image
+        src={image}
+        alt={text}
+        width={200}
+        height={200}
+        className="rounded-lg object-cover"
+      />
+      <span className="text-white">{text}</span>
+      <Link href={buttonLink} className="rounded-lg text-black bg-white">
+        {buttonText} &emsp; right_arrow
+      </Link>
+    </div>
   );
 }
