fe6ef16cc1896cd6cd31dce6092ef2216b767fe6
kpriyanshu2003 <az.priyanshu.rockstar@gmail.com>
2024-03-05 15:50:18 +0530

    Initial Commit. Setup TradingViewWidget

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Sentiment from "@/components/Sentiment";
+import TradingView from "@/components/TradingViewWidget";
+
+export default function Home() {
+  return (
+    <div>
+      {/* <TradingView /> */}
+      <Sentiment />
+    </div>
+  );
+}
