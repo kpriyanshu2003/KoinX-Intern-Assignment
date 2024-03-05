efdec5a4fd3b8f7763839ab9f4a18fde48d8c3e2
kpriyanshu2003 <az.priyanshu.rockstar@gmail.com>
2024-03-06 01:39:06 +0530

    Refactor button component and update navbar links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,18 @@
 import React from "react";
 import Image from "next/image";
 import { Button } from "@/ui/Button";
+import Link from "next/link";
 
 function Navbar() {
   return (
     <div className="flex justify-between items-center px-10 py-5 border">
       <Image src="/assets/icon.svg" height={100} width={100} alt="KoinX Logo" />
       <div className="flex justify-evenly gap-8 font-semibold items-center">
-        <span>Crypto Taxes</span>
-        <span>Free Tools</span>
-        <span>Resource Center</span>
+        <Link href="/">Crypto Taxes</Link>
+        <Link href="/">Free Tools</Link>
+        <Link href="/">Resource Center</Link>
         <Button
-          bgFrom="blue-500"
-          bgVia="blue-600"
-          bgTo="blue-900"
+          gradient="bg-gradient-to-r from-blue-500 via-blue-600 to-blue-900"
           textColor="white"
         >
           Get Started
--- src/ui/Button.tsx
+++ src/ui/Button.tsx
@@ -1,18 +1,12 @@
 import { ButtonsProps } from "@/@types/Buttons";
 import React from "react";
 
-export function Button({
-  bgFrom,
-  bgVia,
-  bgTo,
-  textColor,
-  children,
-}: ButtonsProps) {
+export function Button({ gradient, textColor, children }: ButtonsProps) {
   return (
-    <div
-      className={`py-2 px-3 bg-gradient-to-r from-${bgFrom} via-${bgVia} to-${bgTo} text-${textColor} rounded-lg`}
+    <button
+      className={`py-2 px-3 text-${textColor} ${gradient} rounded-lg border`}
     >
       {children}
-    </div>
+    </button>
   );
 }
