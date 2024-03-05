bd1a7b7b393438a08f3b4fd250d404fd269360f3
kpriyanshu2003 <az.priyanshu.rockstar@gmail.com>
2024-03-06 02:11:04 +0530

    Add Tokenomics component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import Sentiment from "@/components/Sentiment";
 import Team from "@/components/Team";
 import About from "@/components/About";
+import Tokenomics from "@/components/Tokenomics";
+import TradingViewWidget from "@/components/TradingViewWidget";
 
 export default function Home() {
   return (
     <div>
-      {/* <TradingView /> */}
+      <TradingViewWidget />
       <Sentiment />
       <About />
+      <Tokenomics />
       <Team />
     </div>
   );
