7a81c4337648b1ce9751263bb4770a3ff5186d96
kpriyanshu2003 <az.priyanshu.rockstar@gmail.com>
2024-03-07 16:14:40 +0530

    Made UI Components

--- src/ui/Coin.tsx
+++ src/ui/Coin.tsx
@@ -1,15 +1,37 @@
+import { CoinDataProps } from "@/@types/CoinData";
+import Image from "next/image";
 import React from "react";
+import { CardContent, CardSubHeading } from "./Card";
 
-function Coin({ name }: { name: string }) {
+function Coin({
+  coin,
+  className,
+}: {
+  coin: CoinDataProps;
+  className?: string;
+}) {
   return (
-    <div className="border">
-      <>
-        <span className="text-xl">{name}</span>
-        <span className="text-green-400 bg-green-200">+ 50%</span>
-      </>
-      <div className="h-20 w-24 bg-blue-300">
-        <span className="">Coin Info</span>
-      </div>
+    <div className={`border rounded-lg p-4 mx-2 ${className} overflow-hidden `}>
+      <CardContent className="flex gap-3 items-center">
+        <Image
+          src={coin.item.thumb}
+          alt={coin.item.name}
+          width={50}
+          className="rounded-full md:w-10 md:h-10 h-8 w-8"
+          height={50}
+        />
+        <span className="font-semibold text-sm">{coin.item.symbol}</span>
+      </CardContent>
+      <CardSubHeading className="my-1 whitespace-nowrap">
+        {coin.item.data.price}
+      </CardSubHeading>
+      <Image
+        src={coin.item.data.sparkline}
+        alt="sparkline"
+        width={100}
+        height={50}
+        className="w-full"
+      />
     </div>
   );
 }
